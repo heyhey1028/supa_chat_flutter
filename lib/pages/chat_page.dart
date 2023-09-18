6b5289ca8178eb3864a11ddc253eaddd77fa1f4b
heyhey1028 <shoheiogawa1028@gmail.com>
2023-09-18 15:19:34 +0900

    feat: add firebase database

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,27 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:supa_chat_flutter/widgets/app_drawer.dart';
+import 'package:supa_chat_flutter/widgets/message_text_field.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-import '../widgets/app_drawer.dart';
+import '../models/chat_room.dart';
+import '../widgets/message_list.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({
     super.key,
+    this.room,
   });
+
+  final ChatRoom? room;
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
+  late String userId;
+  late ChatRoom room;
+
+  @override
+  void initState() {
+    userId = Supabase.instance.client.auth.currentUser!.id;
+    room = widget.room ?? ChatRoom(userId: userId, name: 'New chat', createdAt: DateTime.now());
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Chat Page'),
+        title: Text(room.name),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final result = await deleteChatRoom(context, roomId: room.id!);
+              if (result != null && context.mounted) {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const ChatPage(),
+                  ),
+                );
+              }
+            },
+            icon: const Icon(Icons.delete),
+          ),
+        ],
       ),
       drawer: const AppDrawer(),
-      body: const Center(
-        child: Text('Chat Page'),
+      body: Column(
+        children: [
+          Expanded(
+            child: MessageList(
+              roomId: room.id,
+            ),
+          ),
+          MessageTextField(
+            onSubmitted: (message) async {
+              // チャットルームがなければ作成
+              if (room.id == null) {
+                final result = await createChatRoom(context, userId: room.userId, roomName: room.name);
+                // チャットルームが作成できたら、roomに代入
+                if (result != null) {
+                  setState(() {
+                    room = result;
+                  });
+                }
+              }
+
+              // メッセージテーブルにレコードを挿入
+              if (context.mounted) await sendChatMessage(context, message: message, room: room, fromBot: false);
+            },
+          ),
+        ],
       ),
     );
+  }
+
+  Future<ChatRoom?> createChatRoom(
+    BuildContext context, {
+    required String roomName,
+    required String userId,
+  }) async {
+    try {
+      final result = await Supabase.instance.client.from('chat_rooms').insert({
+        'room_name': roomName,
+        'user_id': userId,
+      }).select();
+      return ChatRoom.fromJson(result.first);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(e.toString()),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return null;
+    }
+  }
+
+  Future<void> sendChatMessage(
+    BuildContext context, {
+    required String message,
+    required ChatRoom room,
+    required bool fromBot,
+  }) async {
+    try {
+      await Supabase.instance.client.from('chat_messages').insert({
+        'room_id': room.id,
+        'message': message,
+        'sent_by_bot': fromBot,
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(e.toString()),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+    return;
+  }
+
+  Future<ChatRoom?> deleteChatRoom(
+    BuildContext context, {
+    required String roomId,
+  }) async {
+    try {
+      final result = await Supabase.instance.client.from('chat_rooms').delete().eq('room_id', roomId).select();
+      return ChatRoom.fromJson(result.first);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(e.toString()),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return null;
+    }
   }
 }
---
+++ lib/widgets/chat_room_edit_dialog.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../models/chat_room.dart';
+
+class ChatRoomEditDialog extends StatefulWidget {
+  const ChatRoomEditDialog({
+    super.key,
+    required this.room,
+  });
+
+  final ChatRoom room;
+
+  @override
+  State<ChatRoomEditDialog> createState() => _ChatRoomEditDialogState();
+}
+
+class _ChatRoomEditDialogState extends State<ChatRoomEditDialog> {
+  late TextEditingController _controller;
+  String errorMessage = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: widget.room.name);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Edit Chat Room'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+              labelText: 'Chat Room Name',
+            ),
+          ),
+          if (errorMessage.isNotEmpty)
+            Text(
+              errorMessage,
+              style: const TextStyle(
+                color: Colors.red,
+                fontSize: 12,
+              ),
+              maxLines: 4,
+              overflow: TextOverflow.ellipsis,
+            ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () async {
+            final result = await updateChatRoom(
+              roomId: widget.room.id!,
+              newName: _controller.text,
+            );
+            if (context.mounted && result != null) Navigator.of(context).pop();
+          },
+          child: const Text('Save'),
+        ),
+      ],
+    );
+  }
+
+  Future<ChatRoom?> updateChatRoom({
+    required String roomId,
+    required String newName,
+  }) async {
+    try {
+      final result = await Supabase.instance.client.from('chat_rooms').update({'room_name': newName}).eq('room_id', roomId).select();
+      return ChatRoom.fromJson(result.first);
+    } catch (e) {
+      setState(() {
+        errorMessage = e.toString();
+      });
+      return null;
+    }
+  }
+}
